5d63d8405800994267b94eab18a3b4160c464269
Henrik Petersson <44243358+hnrkcode@users.noreply.github.com>
2023-10-22 20:08:36 +0200

    Refactor get_patterns function

--- gameoflife/pattern/blueprint.py
+++ gameoflife/pattern/blueprint.py
@@ -2,42 +2,16 @@ from gameoflife.settings import PATTERN_LIST
 
 
 def get_patterns():
-    """Read patterns from text file."""
-    count = 1
     patterns = {}
-
     with open(PATTERN_LIST) as patterns_file:
-        name = None
-        layout = []
-
+        name, layout = None, []
         for line in patterns_file:
-            # Ignore lines that start with '#'.
             if line[0] == "#":
                 continue
-
-            # Line is a pattern name if it contains letters.
-            is_name = "".join(
-                [i for i in line if not i.isdigit() and i.isalpha()]
-            ).isalpha()
-
-            # Use name as dictionary key.
-            if is_name:
-                name = f"{count}. " + line.strip("\n")
-                patterns[name] = []
-                count += 1
-                continue
-
-            # All other lines that isn't empty are part of the pattern.
-            if line[0] != "\n":
-                result = list(map(int, list(line.strip("\n"))))
-                layout.append(result)
-                continue
-
-            # Add pattern to the key with it's name and reset pattern list.
-            patterns[name] = layout
-            layout = []
-
-        # Add the last pattern to the dictionary.
-        patterns[name] = layout
-
+            if line[0].isalpha():
+                name, layout = f"{len(patterns) + 1}. {line.strip()}", []
+            elif line.strip():
+                layout.append(list(map(int, line.strip())))
+            if name and layout:
+                patterns[name] = layout
     return patterns
